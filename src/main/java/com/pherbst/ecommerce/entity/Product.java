133f27b482c633fc836c9e3f11dbd83d5cd71fa3
Patrick Herbst <p.herbst.1987@gmail.com>
2024-01-02 11:37:59 +0100

    Entities Product and ProductCategory setup

---
+++ src/main/java/com/pherbst/ecommerce/entity/Product.java
@@ -0,0 +1,54 @@
+package com.pherbst.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data  //Lombok's annotation to reduce boilerplate code (getters & setters)
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp // Hibernate will automatically manage the timestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp // Hibernate will automatically manage the timestamp
+    private Date lastUpdated;
+}
