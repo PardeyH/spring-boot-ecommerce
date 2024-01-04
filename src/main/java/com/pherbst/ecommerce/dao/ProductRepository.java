af2dec79df61cafa28da10876840e71c8e1dd630
Patrick Herbst <p.herbst.1987@gmail.com>
2024-01-04 11:52:05 +0100

    added CrossOrigin annotation to the repositories for product and productCategory

--- src/main/java/com/pherbst/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/pherbst/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.pherbst.ecommerce.dao;
 
 import com.pherbst.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // accepts calls from web browser scripts for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
